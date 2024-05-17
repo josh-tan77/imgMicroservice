9be16e7d818f1bad44af4a2933886b754fb199a6
josh-tan-bt <josh.tan@bio-techne.com>
2024-05-17 15:06:18 -0700

    testing

--- app.js
+++ app.js
@@ -25,17 +25,19 @@ app.get('/', (req, res) => {
     url = url + "?origin=*";
     Object.keys(params).forEach(function(key){url += "&" + key + "=" + params[key];});
     
-    fetch(url)
-        .then(function(response){return response.json();})
-        .then(function(response) {
-            var pages = response.query.pages;
-            for (var page in pages) {
-                for (var img of pages[page].images) {
-                    console.log(img.title);
-                }
-            }
-        })
-        .catch(function(error){console.log(error);});
+    // fetch(url)
+    //     .then(function(response){return response.json();})
+    //     .then(function(response) {
+    //         var pages = response.query.pages;
+    //         for (var page in pages) {
+    //             for (var img of pages[page].images) {
+    //                 console.log(img.title);
+    //             }
+    //         }
+    //     })
+    //     .catch(function(error){console.log(error);});
+
+    res.send("Hello")
 })
 
 // check if it contains search term
