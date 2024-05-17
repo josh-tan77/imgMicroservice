20ac1957c40ac435af319d625ffd7f3428fee982
josh-tan-bt <josh.tan@bio-techne.com>
2024-05-17 15:15:01 -0700

    testing

--- app.js
+++ app.js
@@ -11,33 +11,34 @@ if (port == null || port == "") {
 
 // get request to index page
 app.get('/', (req, res) => {
-    // var url = "https://en.wikipedia.org/w/api.php"; 
+    var url = "https://en.wikipedia.org/w/api.php"; 
 
-    // var params = {
-    //     action: "query",
-    //     prop: "images",
-    //     titles: "Albert Einstein",
-    //     format: "json"
-    // };
+    var params = {
+        action: "query",
+        prop: "images",
+        titles: "Albert Einstein",
+        format: "json"
+    };
 
-    // console.log(params)
+    console.log(params)
     
-    // url = url + "?origin=*";
-    // Object.keys(params).forEach(function(key){url += "&" + key + "=" + params[key];});
+    url = url + "?origin=*";
+    Object.keys(params).forEach(function(key){url += "&" + key + "=" + params[key];});
     
-    // fetch(url)
-    //     .then(function(response){return response.json();})
-    //     .then(function(response) {
-    //         var pages = response.query.pages;
-    //         for (var page in pages) {
-    //             for (var img of pages[page].images) {
-    //                 console.log(img.title);
-    //             }
-    //         }
-    //     })
-    //     .catch(function(error){console.log(error);});
+    fetch(url)
+        .then(function(response){return response.json();})
+        .then(function(response) {
+            var pages = response.query.pages;
+            for (var page in pages) {
+                for (var img of pages[page].images) {
+                    console.log(img.title);
+                }
+            }
+        })
+        .catch(function(error){console.log(error);});
 
-    res.send("Hello")
+    res.send(pages.json)
+    // res.send("Hello")
 })
 
 // check if it contains search term
