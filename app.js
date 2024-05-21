549bbff5422cce5c1554683c64c3e7f2806ee73b
josh-tan-bt <josh.tan@bio-techne.com>
2024-05-21 14:00:17 -0700

    testing

--- app.js
+++ app.js
@@ -8,48 +8,75 @@ if (port == null || port == "") {
 }
 
 
-
 // get request to index page
-app.get('/', async (req, res) => {
-    var url = "https://en.wikipedia.org/w/api.php"; 
-    var images;
+app.get('/:keyword', async (req, res) => {
+
+    console.log(req.params);
+    var keyword = req.params.keyword;
+
+    var source_url = "https://en.wikipedia.org/w/api.php"; 
+    var titles = [];
+    var data = {};
 
     var params = {
         action: "query",
-        prop: "imageinfo", 
-        iiprop: "url",
-        titles: "Albert Einstein",
-        format: "json"
+        prop: "images", 
+        titles: keyword,
+        format: "json",
+        imlimit: 500
     };
-
-    console.log(params)
     
-    url = url + "?origin=*";
+    url = source_url + "?origin=*";
     Object.keys(params).forEach(function(key){url += "&" + key + "=" + params[key];});
+
+    // First, get a list of all iamge titles on the page
     
     await fetch(url)
         .then(function(response){return response.json();})
         .then(function(response) {
-            // var pages = response.query.pages;
-            images = response.query.pages;
-            // for (var page in pages) {
-            //     for (var img of pages[page].images) {
-            //         console.log(img.title);
-            //     }
-            // }
+            var pages = response.query.pages;
+            
+            for (var page in pages) {
+                for (var img of pages[page].images) {
+                    console.log(img.title);
+                    var title = img.title;
+                    titles.push(title);
+
+                }
+            }
         })
         .catch(function(error){console.log(error);});
+    console.log(titles);
 
-    console.log(images)
-    res.send("Hello")
+    // Then, get the url for each image by title
+    for (let i = 0; i < titles.length; i++) {
+        console.log(titles[i]);
+        var params2 = {
+            action: "query",
+            prop: "imageinfo", 
+            titles: titles[i],
+            format: "json",
+            iiprop: "url"
+        };
+
+
+        url2 = source_url + "?origin=*";
+        Object.keys(params2).forEach(function(key){url2 += "&" + key + "=" + params2[key];});
+
+        await fetch(url2)
+        .then(function(response){return response.json();})
+        .then(function(response) {
+            var pages = response.query.pages;
+            for (var p in pages) {
+                data[titles[i]] = pages[p].imageinfo[0].url;
+            }
+        })
+        .catch(function(error){console.log(error);});
+    }
+
+    console.log(data);
+    res.send(JSON.stringify(data));
 })
-
-// check if it contains search term
-
-// if yes send response
-
-
-// otherwise send nothing
 
 /*
     LISTENER
